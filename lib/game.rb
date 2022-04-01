628a844f557ce7e846a953dda8b46552133aff6b
David Bielik <davidbielik@gmail.com>
2022-04-01 14:18:54 -0400

    added switch_player method

--- lib/game.rb
+++ lib/game.rb
@@ -14,37 +14,36 @@ attr_accessor :current_player
     @current_player
   end
 
-  def select_token(num)
-    if num=="1"
-      token="★"
-    elsif num=="2"
-      token="♥"
-    elsif num=="3"
-      token="⚡"
-    elsif num=="4"
-      token="⚽"
-    elsif num=="5"
-      token="⚾"
-    else
-      puts "Not a valid choice"
-    end
-  end
-    
-  def determine_players(player=player_1)
+  def determine_players
     puts "Who is player 1? (enter your name)"
     name=gets.chomp
-    puts "Which token would you like to use? (enter the number)\n
-    1.) ★\n
-    2.) ♥\n
-    3.) ⚡\n
-    4.) ⚽\n
-    5.) ⚾" 
-    while token==nil
-        choice=gets.chomp
-        token=select_token(choice)
+    puts "Which letter would you like to use as your token? (enter a letter)"
+    token=gets.chomp
+    @player_1 = Player.new(name, token)
+    
+    
+    
+    puts "Who is player 2? (enter your name)"
+    name=gets.chomp
+    puts "Which letter would you like to use as your token? (enter a letter)"
+    token=gets.chomp
+    @player_2 = Player.new(name, token)
+  end
+
+  def switch_player
+    if @current_player==@player_1
+        current_player=@player_2
+    else
+        current_player=@player_1
     end
-    player = Player.new(name, token)
-    determine_players(player_2)
+  end
+
+  def take_turns
+    switch_player
+    puts @board
+    puts "Make your selction by entering a row number."
+    move = gets.chomp
+    #this is unfinished - need to work on finishing board class first"
   end
 end
 
